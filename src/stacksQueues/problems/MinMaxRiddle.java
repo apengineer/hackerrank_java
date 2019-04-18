ac3674126efb0e1ebe315aaf26e5bf89add6ee2e
Aswathy Prasad <33281453+apengineer@users.noreply.github.com>
2019-04-18 17:43:27 +0200

    updated code with queue

--- src/stacksQueues/problems/MinMaxRiddle.java
+++ src/stacksQueues/problems/MinMaxRiddle.java
@@ -1,8 +1,12 @@
-package stacksQueues.problems;
+package hackerrank.stacksAndQueues;
 
+
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class MinMaxRiddle {
 
@@ -11,29 +15,71 @@ public class MinMaxRiddle {
 		if (array == null) return null;
 		int length = array.length;
 
+		long tmp[] = new long[length - 1];		
 		long res[] = new long[length];
-		PriorityQueue<Long> pq = new PriorityQueue<Long>();
-		int k = 1;
 		
 		long max = Arrays.stream(array).reduce((x, y) -> x > y ? x : y).getAsLong();
 		
-		pq.add(max);		
-		
+		Queue<Long> q = new LinkedList<Long>();
+		q.add(max);
 		for (int i = 0; i < length - 1; i++) {
-			res[i] = Math.min(array[i], array[i + 1]);
-			pq.add(Math.min(array[i], array[i + 1]));
-		}
+			tmp[i] = Math.min(array[i], array[i + 1]);
+			q.add(tmp[i]);		
+		}			
+				
+		res[0] = q.poll();
+		
+		int i = 0; int end = array.length - 1;
+		int index = 1; long curMax = 0; long curMin = 0;
+		
+		/*while(!q.isEmpty()) {
+			System.out.print(q.poll() + " ");
+		}*/
 	
-		for (int i = 0; i < length ; i++) {
-			res[i]  = pq.poll();			
+		while(i < end) {
+			long first = q.poll();
+			if ( i <= end - 1) {
+				long second = q.peek();
+				
+				curMin = Math.min(first, second);
+				
+				q.add(curMin);			
+				
+				
+				curMax = Math.max(first, curMax);
+				
+				i++;
+				
+				if (i ==  end) {
+					res[index] = curMax;
+					index++;
+					
+					i = 0;
+					end = end - 1 ;
+				}		
+			}
+			
+			
 		}
+		
+	/*	while(!q.isEmpty()) {
+			System.out.print(q.poll() + " ");
+		}*/
+		
+		
+	
+		
+		
+		
+		
+		
 
 		return res;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		long[] array = {6, 3, 5, 1, 12};
+		long[] array = {1, 2, 3, 5, 1, 13, 3};
 		long[] res = riddle(array);
 		System.out.println();
 		for(long l : res) {
